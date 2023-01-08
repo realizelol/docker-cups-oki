fa7e0ea585d2e90157eb10c2dfc13daab95f3029
realizelol <bsw.bsw@gmx.de>
2023-01-08 03:22:28 +0100

    [Update] Git-Workflow ++ Run Upgrade in background, until connected.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -e
-set -x
+#set -x ## debug
 
 if [[ ! -f /etc/cups/cupsd.conf ]]; then
   cp -rpn /etc/cups-skel/* /etc/cups/
 fi
 
-# update packages if connected to www
-if ping -c3 -W3 archlinux.org &>/dev/null; then
-  pacman -Syy  --noconfirm
-  pacman -Syyu --noconfirm
-  pacman -Fyy  --noconfirm
-fi
+# update packages in background if connected to www
+( ( while ! ping -c3 -W3 archlinux.org; do sleep 2; done; \
+    pacman -Syy  --noconfirm && \
+    pacman -Syyu --noconfirm && \
+    pacman -Fyy  --noconfirm \
+)& )>/dev/null 2>&1
 
 # fix papersize
 echo 'a4' > /etc/papersize
 
 # run cupsd
 echo "$(date +'%Y-%m-%d - %H:%M:%S') - cups started successfully."
-exec "$@"
+exec "${@}"
